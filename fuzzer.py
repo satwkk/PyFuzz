df0537a5fb70985b16bfb46db3481056c635aafa
Satweek Tripathy <smoxics@gmail.com>
2023-03-15 20:19:11 +0530

    random refactoring

--- fuzzer.py
+++ fuzzer.py
@@ -1,20 +1,10 @@
 from enum import Enum
-from typing import List
+from typing import List, Union
 from abc import ABC, abstractmethod, abstractproperty
 
 import parse
 import requests
 import threading
-
-class EArgType(Enum):
-    NONE = 0
-    URL = 1
-    HEADER = 2
-    
-class ERequestType(Enum):
-    NONE = 0
-    GET = 1
-    POST = 2
 
 class Fuzzer(ABC):
     ''' Base fuzzer class '''
@@ -25,20 +15,39 @@ class Fuzzer(ABC):
         self.headers = parse.craft_headers(kwargs.get('headers'))
         self.wordlist = open(kwargs.get('wordlist'), 'r')
         self.timeout = kwargs.get('timeout')
-        self.filter_by = kwargs.get('filter_by')
+        self.filter_by_status_code = kwargs.get('filter_by_status_code')
+        self.filter_by_size = kwargs.get('filter_by_size')
         self.session = requests.Session()
         self.args_to_fuzz = [self.prepare_args(word) for word in self.wordlist.read().strip().split('\n')]
         self.response = dict()
-        self.request_mode = ERequestType.GET
         
+    '''
+    Prepares the fuzzing arguments and caches them into `self.args_to_fuzz` inside constructor.
+    @param: word_to_replace - Word from wordlist to replace with FUZZ keyword.
+    @return: str            - Returns the replaced argument
+    '''
     @abstractmethod
     def prepare_args(self, word_to_replace: str) -> str: ...
 
+    '''
+    Sends the HTTP request with fuzzing argument.
+    @param: fuzz_arg - Each args inside `self.final_arg`
+    @param: out_response - A dictionary to cache responses for later use.
+    @return: None
+    '''
     @abstractmethod
     def make_request(self, fuzz_arg: str, out_responses: dict[str, requests.Response]) -> None: ...
     
+    '''
+    Returns the final fuzzing argument.
+    @param: None
+    @return: List of URLs replaced with words from wordlist at FUZZ index.
+    '''
     @abstractproperty
-    def final_args(self): ...
+    def final_args(self) -> Union[List[str], List[dict[str, str]]]: ...
+    
+    # TODO
+    def filter_by(self): ...
     
     def start(self) -> None:
         for arg in self.final_args:
@@ -50,44 +59,27 @@ class Fuzzer(ABC):
         self.wordlist.close()
 
 class URLFuzzer(Fuzzer):
-    ''' Child class of Fuzzer which bruteforces URLs and endpoints'''
+    ''' Child class of Fuzzer which bruteforces URLs and endpoints '''
     
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
     
-    '''
-    Returns the final fuzzing argument, in this case it returns all the URLs.
-    @param: None
-    @return: List of URLs replaced with words from wordlist at FUZZ index.
-    '''
     @property
     def final_args(self) -> List[str]:
         return self.args_to_fuzz
     
-    '''
-    Prepares the fuzzing arguments and caches them into `self.args_to_fuzz` inside constructor.
-    In this case it replaces all the FUZZ instances in URLs with words from wordlist
-    @param: word_to_replace - Word from wordlist to replace with FUZZ keyword.
-    @return: str            - Returns the replaced argument
-    '''
     def prepare_args(self, word_to_replace: str) -> str:
         url, fuzz_idx = self.fuzz_arg['url']
         final_arg = f'{url[:fuzz_idx]}{word_to_replace}{url[fuzz_idx+4:]}'
         return final_arg
 
-    '''
-    Sends the HTTP request with respective `fuzz_arg` which is passed to the url parameter in `self.session.get`.
-    @param: fuzz_arg - The arg inside `self.final_arg`
-    @param: out_response - A dictionary to cache responses for later use.
-    @return: None
-    '''
     def make_request(self, fuzz_arg: str, out_responses: dict[str, requests.Response]) -> None:
         try:
             response = requests.get(url=fuzz_arg, headers=self.headers, allow_redirects=False)
             # TODO: Filtering by status code, line, word and size.
             if response.status_code == 200:
                 print(f'{fuzz_arg} => Code [{response.status_code}]')
-        except Exception as e:
+        except:
             pass
     
 class HeaderFuzzer(Fuzzer):
@@ -96,46 +88,30 @@ class HeaderFuzzer(Fuzzer):
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
     
-    '''
-    Returns the final fuzzing argument, in this case it returns all the headers.
-    @param: None
-    @return: List of headers replaced with words from wordlist at FUZZ index.
-    '''
     @property
     def final_args(self) -> List[dict[str, str]]:
         return [parse.craft_headers(arg) for arg in self.args_to_fuzz]
     
-    '''
-    Prepares the fuzzing arguments and caches them into `self.args_to_fuzz` inside constructor.
-    In this case it replaces all the FUZZ instances in header with words from wordlist
-    @param: word_to_replace - Word from wordlist to replace with FUZZ keyword.
-    @return: str            - Returns the replaced argument
-    '''
     def prepare_args(self, word_to_replace: str) -> str:
         header, fuzz_idx = self.fuzz_arg['headers']
         final_arg = f'{header[:fuzz_idx]}{word_to_replace}{header[fuzz_idx+4:]}'
         return final_arg
-    
-    '''
-    Sends the HTTP request with respective `fuzz_arg` which is passed to the header parameter in `self.session.get`.
-    @param: fuzz_arg - The arg inside `self.final_arg`
-    @param: out_response - A dictionary to cache responses for later use.
-    @return: None
-    '''
+
     def make_request(self, fuzz_arg: str, out_responses: dict[str, requests.Response]) -> None:
         try:
             response = requests.get(url=self.url, headers=fuzz_arg, allow_redirects=False)
             # TODO: Filtering by status code, line, word and size.
             if response.status_code == 200:
                 print(f'{fuzz_arg} => Code [{response.status_code}]')
-        except Exception as e:
+        except:
             pass
         
-
-def get_fuzzer(parsed_args, **kwargs) -> Fuzzer:
+def get_fuzzer(parsed_args) -> Fuzzer:
     ''' Fuzzer factory to return respective fuzzer based on key in parsed_arg dictionary '''
     
     if 'url' in parsed_args:
-        return URLFuzzer(**kwargs)
+        return URLFuzzer
     elif 'headers' in parsed_args:
-        return HeaderFuzzer(**kwargs)
+        return HeaderFuzzer
+
+
--- pyfuzz.py
+++ pyfuzz.py
@@ -13,19 +13,22 @@ if __name__ == '__main__':
     parser.add_option("-w", "--wordlist", dest="wordlist", help="Wordlist for bruteforcing")
     parser.add_option("-H", "--headers", dest="headers", help="Headers to provide for http/https request", )
     parser.add_option("-t", "--timeout", dest="timeout", help="Timeout for http/https request to complete")
+    parser.add_option("--fc", "--filter-by-status", dest="filter_by_status_code", help="Exclude responses with specified status code")
+    parser.add_option("--fs", "--filter-by-size", dest="filter_by_size", help="Exclude responses with specified response size")
 
     option, args = parser.parse_args()
 
     parsed_args = parse.parse_args(parser, option)
     
-    fuzzer = get_fuzzer(
-        parsed_args=parsed_args, 
+    fuzzer = get_fuzzer(parsed_args=parsed_args) (
         args=parsed_args, 
-        url=option.url, 
+        url=option.url,
         headers=option.headers if option.headers else DEFAULT_HEADERS, 
         wordlist=option.wordlist, 
-        timeout=int(option.timeout) if option.timeout else None
-        )
+        timeout=int(option.timeout) if option.timeout else None,
+        filter_by_status_code=int(option.filter_by_status_code) if option.filter_by_status_code else None,
+        filter_by_size=int(option.filter_by_size) if option.filter_by_size else None
+    )
     
     fuzzer.start()
     fuzzer.cleanup()
